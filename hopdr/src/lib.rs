9bd2fe95cf3cf1e219011b7b4f0de6fed4389114
katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2022-04-15 16:28:45 +0900

    Enable debug! output even when testing

--- hopdr/src/lib.rs
+++ hopdr/src/lib.rs
@@ -9,3 +9,14 @@ pub mod preprocess;
 pub mod solver;
 #[macro_use]
 pub mod util;
+
+
+#[cfg(test)]
+#[ctor::ctor]
+fn init() {
+    env_logger::builder()
+        .format_timestamp(None)
+        .format_module_path(true)
+        .format_level(false)
+        .init();
+}
