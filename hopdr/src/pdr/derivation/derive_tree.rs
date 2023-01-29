8f885f6fa07343e78b55b57552e0f14ed41d5713
katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2023-01-29 23:43:05 +0900

    Add derive_tree

---
+++ hopdr/src/pdr/derivation/derive_tree.rs
@@ -0,0 +1,17 @@
+use super::{Ty, G};
+use crate::util::P;
+
+enum DeriveNodeKind {
+    Conj(P<DeriveNode>, P<DeriveNode>),
+    Disj(P<DeriveNode>, P<DeriveNode>),
+    Abs(P<DeriveNode>),
+    App(P<DeriveNode>, P<DeriveNode>),
+    Forall(P<DeriveNode>),
+    Atom(P<DeriveNode>),
+}
+
+struct DeriveNode {
+    rule: DeriveNodeKind,
+    expr: G,
+    ty: Ty,
+}
