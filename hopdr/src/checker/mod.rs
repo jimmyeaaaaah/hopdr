b90bf4db5d9de6594e1f465ba3fea2fdf431084b
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2023-11-09 21:37:54 +0900

    Update translation

--- hopdr/src/checker/mod.rs
+++ hopdr/src/checker/mod.rs
@@ -1,5 +1,5 @@
 mod executor;
-use crate::formula::hes::{Goal, Problem};
+use crate::formula::hes::{Goal, GoalKind, Problem};
 use crate::formula::{Bot, Constraint, Ident, Negation, Op, Type as HFLType};
 use crate::ml::{optimize, Expr, Function, Program, Type as SType, Variable};
 use crate::preprocess::Context;
@@ -17,6 +17,7 @@ impl<'a> Config<'a> {
 struct Translator<'a> {
     config: Config<'a>,
 }
+type G = Goal<Constraint>;
 impl<'a> Translator<'a> {
     fn new(config: Config<'a>) -> Self {
         Self { config }
@@ -43,6 +44,35 @@ impl<'a> Translator<'a> {
         let body = f(p);
         Expr::mk_fun(v, body)
     }
+    fn handle_conj_disj(&self, g11: &G, g12: &G, g21: &G, g22: &G) -> Option<Expr> {
+        let s11: Option<Constraint> = g11.clone().into();
+        let s12: Option<Constraint> = g12.clone().into();
+        let s21: Option<Constraint> = g21.clone().into();
+        let s22: Option<Constraint> = g22.clone().into();
+
+        let (c1, g1) = match (s11, s12) {
+            (Some(c), _) => (c, g12),
+            (_, Some(c)) => (c, g11),
+            _ => return None,
+        };
+        let (c2, g2) = match (s21, s22) {
+            (Some(c), _) => (c, g22),
+            (_, Some(c)) => (c, g21),
+            _ => return None,
+        };
+
+        if c1.negate().unwrap() == c2 {
+            Some(Translator::gen_prop(move |p| {
+                let g1 = self.translate_goal(g1.clone());
+                let g2 = self.translate_goal(g2.clone());
+                let g1 = Expr::mk_app(g1.clone(), Expr::mk_var(p));
+                let g2 = Expr::mk_app(g2.clone(), Expr::mk_var(p));
+                Expr::mk_if(c2, g1, g2)
+            }))
+        } else {
+            None
+        }
+    }
     // [θ] = fun p -> if not [θ] then raise FalseExc
     // [\x. Ψ] = fun x -> [Ψ]
     // [Ψ1 Ψ2] = [Ψ1] [Ψ2]
@@ -53,27 +83,27 @@ impl<'a> Translator<'a> {
     // [Ψ1 \/ Ψ2] = fun p -> try [Ψ1]p with FalseExc -> [Ψ2]p
     // [(¬θ \/ Ψ1) /\ (θ \/ Ψ2)] = fun p -> if [θ] then [Ψ1] else  [Ψ2]
     // [∀x. Ψ] = fun p -> let x = * in [Ψ] p
-    fn translate_goal(&self, goal: Goal<Constraint>) -> Expr {
+    fn translate_goal(&self, goal: G) -> Expr {
         match goal.kind() {
-            crate::formula::hes::GoalKind::Constr(c) => {
+            GoalKind::Constr(c) => {
                 Self::gen_prop(|_| Expr::mk_if(c.clone(), Expr::mk_unit(), Expr::mk_raise()))
             }
-            crate::formula::hes::GoalKind::Op(_) => {
+            GoalKind::Op(_) => {
                 unreachable!()
             }
-            crate::formula::hes::GoalKind::Var(x) => Expr::mk_var(*x),
-            crate::formula::hes::GoalKind::Abs(x, g) => {
+            GoalKind::Var(x) => Expr::mk_var(*x),
+            GoalKind::Abs(x, g) => {
                 let body = self.translate_goal(g.clone());
                 let v = Variable::mk(x.id, self.translate_type(&x.ty));
                 Expr::mk_fun(v, body)
             }
-            crate::formula::hes::GoalKind::App(g1, g2) => {
+            GoalKind::App(g1, g2) => {
                 let g1 = self.translate_goal(g1.clone());
                 // TODO: check the type of g1 so that we can infer g2 is op or not
                 // corner case: g2 is a variable
                 // I think they are preprocessed but I forgot it
                 match g2.kind() {
-                    crate::formula::hes::GoalKind::Op(o) => Expr::mk_iapp(g1, o.clone()),
+                    GoalKind::Op(o) => Expr::mk_iapp(g1, o.clone()),
                     _ => {
                         let g2 = self.translate_goal(g2.clone());
                         Expr::mk_app(g1, g2)
@@ -81,7 +111,17 @@ impl<'a> Translator<'a> {
                 }
             }
             //
-            crate::formula::hes::GoalKind::Conj(g1_fml, g2_fml) => Self::gen_prop(|p| {
+            GoalKind::Conj(g1_fml, g2_fml) => Self::gen_prop(|p| {
+                match (g1_fml.kind(), g2_fml.kind()) {
+                    (GoalKind::Disj(g11, g12), GoalKind::Disj(g21, g22)) => {
+                        match self.handle_conj_disj(g11, g12, g21, g22) {
+                            Some(x) => return x,
+                            _ => (),
+                        }
+                    }
+                    _ => (),
+                };
+
                 let g1 = self.translate_goal(g1_fml.clone());
                 let g1 = Expr::mk_app(g1, Expr::mk_var(p));
                 let g2 = self.translate_goal(g2_fml.clone());
@@ -100,12 +140,12 @@ impl<'a> Translator<'a> {
                     Expr::mk_letrand(ident, body)
                 }
             }),
-            crate::formula::hes::GoalKind::Disj(g1, g2) => Self::gen_prop(|p| {
+            GoalKind::Disj(g1, g2) => Self::gen_prop(|p| {
                 let g1 = self.translate_goal(g1.clone());
                 let g2 = self.translate_goal(g2.clone());
                 Expr::mk_app(g1, Expr::mk_app(g2, Expr::mk_var(p)))
             }),
-            crate::formula::hes::GoalKind::Univ(v, g) => Self::gen_prop(|p| {
+            GoalKind::Univ(v, g) => Self::gen_prop(|p| {
                 let body = self.translate_goal(g.clone());
                 Expr::mk_letrand(v.id, Expr::mk_app(body, Expr::mk_var(p)))
             }),
