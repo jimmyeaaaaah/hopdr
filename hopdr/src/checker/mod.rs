042c3648c721d9479ac85ea4e0469713b63d118b
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2023-11-09 17:49:59 +0900

    Fix translation & Add conj-e

--- hopdr/src/checker/mod.rs
+++ hopdr/src/checker/mod.rs
@@ -1,5 +1,5 @@
 use crate::formula::hes::{Goal, Problem};
-use crate::formula::{Bot, Constraint, Ident, Op, Type as HFLType};
+use crate::formula::{Bot, Constraint, Ident, Negation, Op, Type as HFLType};
 use crate::ml::{Expr, Function, Program, Type as SType, Variable};
 use crate::preprocess::Context;
 
@@ -42,21 +42,20 @@ impl<'a> Translator<'a> {
         let body = f(p);
         Expr::mk_fun(v, body)
     }
-    // [θ] = fun p -> θ || p
+    // [θ] = fun p -> if not [θ] then raise FalseExc
     // [\x. Ψ] = fun x -> [Ψ]
     // [Ψ1 Ψ2] = [Ψ1] [Ψ2]
     // [Ψ a] = [Ψ] a
-    // [Ψ1 /\ Ψ2] = fun p -> if * then [Ψ1]p else [Ψ2]p
-    // [Ψ1 \/ Ψ2] = fun p -> [Ψ1] ([Ψ2] p)
+    // [Ψ1 /\ Ψ2] = fun p -> if * then [Ψ1] p else [Ψ2]p
+    // [θ /\ Ψ2] = fun p -> [θ] p; [Ψ2]p
+    // [Ψ1 /\ θ] = fun p -> [θ] p; [Ψ1]p
+    // [Ψ1 \/ Ψ2] = fun p -> try [Ψ1]p with FalseExc -> [Ψ2]p
+    // [(¬θ \/ Ψ1) /\ (θ \/ Ψ2)] = fun p -> if [θ] then [Ψ1] else  [Ψ2]
     // [∀x. Ψ] = fun p -> let x = * in [Ψ] p
-    // [X] = X
-    //
-    // [X =u Ψ] = let rec X = [Ψ]
-    // [({X=u Ψ}, φ)] = [X=u Ψ] in assert([φ] false)
     fn translate_goal(&self, goal: Goal<Constraint>) -> Expr {
         match goal.kind() {
             crate::formula::hes::GoalKind::Constr(c) => {
-                Self::gen_prop(|p| Expr::mk_or(Expr::mk_constraint(c.clone()), Expr::mk_var(p)))
+                Self::gen_prop(|_| Expr::mk_if(c.clone(), Expr::mk_unit(), Expr::mk_raise()))
             }
             crate::formula::hes::GoalKind::Op(_) => {
                 unreachable!()
@@ -77,15 +76,25 @@ impl<'a> Translator<'a> {
                     }
                 }
             }
-            crate::formula::hes::GoalKind::Conj(g1, g2) => Self::gen_prop(|p| {
-                let g1 = self.translate_goal(g1.clone());
+            //
+            crate::formula::hes::GoalKind::Conj(g1_fml, g2_fml) => Self::gen_prop(|p| {
+                let g1 = self.translate_goal(g1_fml.clone());
                 let g1 = Expr::mk_app(g1, Expr::mk_var(p));
-                let g2 = self.translate_goal(g2.clone());
+                let g2 = self.translate_goal(g2_fml.clone());
                 let g2 = Expr::mk_app(g2, Expr::mk_var(p));
                 let ident = Ident::fresh();
                 let c = Constraint::mk_geq(Op::mk_var(ident), Op::zero());
-                let body = Expr::mk_if(c, g1, g2);
-                Expr::mk_letrand(ident, body)
+
+                //[θ /\ Ψ2] = fun p -> [θ] p; [Ψ2]p
+                //[Ψ1 /\ θ] = fun p -> [θ] p; [Ψ1]p
+                if Into::<Option<Constraint>>::into(g1_fml.clone()).is_some() {
+                    Expr::mk_sequential(g1, g2)
+                } else if Into::<Option<Constraint>>::into(g2_fml.clone()).is_some() {
+                    Expr::mk_sequential(g2, g1)
+                } else {
+                    let body = Expr::mk_if(c, g1, g2);
+                    Expr::mk_letrand(ident, body)
+                }
             }),
             crate::formula::hes::GoalKind::Disj(g1, g2) => Self::gen_prop(|p| {
                 let g1 = self.translate_goal(g1.clone());
@@ -110,10 +119,7 @@ impl<'a> Translator<'a> {
             })
             .collect();
         let main = self.translate_goal(problem.top.clone());
-        let main = Expr::mk_assert(Expr::mk_app(
-            main,
-            Expr::mk_constraint(Constraint::mk_false()),
-        ));
+        let main = Expr::mk_app(main, Expr::mk_unit());
         Program {
             functions,
             main,
