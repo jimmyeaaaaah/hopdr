ffc5d799cb7d5ee9682be3ed2c7cd9459944aa19
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2024-02-26 18:52:10 +0900

    Add simplify constraints optimization

--- hopdr/src/ml/peephole.rs
+++ hopdr/src/ml/peephole.rs
@@ -106,8 +106,52 @@ pub(super) fn peephole_optimize<'a>(mut p: Program<'a>) -> Program<'a> {
             }
         }
     }
+
+    // simplify constraints
+    fn simplify_constraints(e: &Expr) -> Expr {
+        match e.kind() {
+            ExprKind::Var(_) | ExprKind::Raise | ExprKind::Unit => e.clone(),
+            ExprKind::Constraint(c) => Expr::mk_constraint(c.simplify()),
+            ExprKind::Or(e1, e2) => Expr::mk_or(simplify_constraints(e1), simplify_constraints(e2)),
+            ExprKind::And(e1, e2) => {
+                Expr::mk_and(simplify_constraints(e1), simplify_constraints(e2))
+            }
+            ExprKind::App(e1, e2) => {
+                Expr::mk_app(simplify_constraints(e1), simplify_constraints(e2))
+            }
+            ExprKind::IApp(e1, e2) => Expr::mk_iapp(simplify_constraints(e1), e2.simplify()),
+            ExprKind::Fun { ident, body } => {
+                Expr::mk_fun(ident.clone(), simplify_constraints(body))
+            }
+            ExprKind::Op(o) => Expr::mk_op(o.simplify()),
+            ExprKind::If { cond, then, els } => Expr::mk_if(
+                simplify_constraints(cond),
+                simplify_constraints(then),
+                simplify_constraints(els),
+            ),
+            ExprKind::LetRand { ident, range, body } => {
+                Expr::mk_letrand(ident.clone(), range.clone(), simplify_constraints(body))
+            }
+            ExprKind::TryWith { body, handler } => {
+                Expr::mk_try_with(simplify_constraints(body), simplify_constraints(handler))
+            }
+            ExprKind::Assert(e) => Expr::mk_assert(simplify_constraints(e)),
+            ExprKind::Sequential { lhs, rhs } => {
+                Expr::mk_sequential(simplify_constraints(lhs), simplify_constraints(rhs))
+            }
+            ExprKind::Tuple(args) => {
+                let args = args.iter().map(simplify_constraints).collect();
+                Expr::mk_tuple(args)
+            }
+            ExprKind::LetTuple { idents, body, cont } => Expr::mk_let_tuple(
+                idents.clone(),
+                simplify_constraints(body),
+                simplify_constraints(cont),
+            ),
+        }
+    }
     fn translate_expr(e: &Expr) -> Expr {
-        let mut e = e.clone();
+        let mut e = simplify_constraints(e);
         for _ in 0..OPTIMIZE_LIMIT {
             let e2 = f(&e);
             if e == e2 {
