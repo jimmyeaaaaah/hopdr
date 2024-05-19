cabb6c5c141e411b44d5922cb54fd0006d8d4d04
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2024-05-19 19:38:32 +0900

    Handle nested if precisely

--- hopdr/src/preprocess/find_ite.rs
+++ hopdr/src/preprocess/find_ite.rs
@@ -25,22 +25,30 @@ fn contains_same_fv(g: &Goal, g2: &Goal) -> bool {
 }
 
 impl FindITETransform {
-    fn find_match(&self, gs: &Vec<Goal>, gs2: &Vec<Goal>) -> Option<(Constraint, Goal, Goal)> {
-        fn aux(gs: &Vec<Goal>, idx: usize) -> Goal {
-            gs.iter()
-                .enumerate()
-                .filter(|(k, _)| *k != idx)
-                .fold(Goal::mk_false(), |acc, (_, g)| {
-                    Goal::mk_disj_opt(acc, g.clone())
-                })
-        }
+    fn disj_except_for(gs: &Vec<Goal>, idx: usize) -> (Goal, Goal) {
+        let g_aux = gs[idx].clone();
+        let g = gs
+            .iter()
+            .enumerate()
+            .filter(|(k, _)| *k != idx)
+            .fold(Goal::mk_false(), |acc, (_, g)| {
+                Goal::mk_disj_opt(acc, g.clone())
+            });
+        (g, g_aux)
+    }
+    // (c \/ g1) /\ (not c \/ g2) -> (c as constr, g1, g2, c, not c)
+    fn find_match(
+        &self,
+        gs: &Vec<Goal>,
+        gs2: &Vec<Goal>,
+    ) -> Option<(Constraint, Goal, Goal, Goal, Goal)> {
         for (i, g1) in gs.iter().enumerate() {
             for (j, g2) in gs2.iter().enumerate() {
                 if contains_same_fv(g1, g2) && check_dual(g1, g2, self.lightweight) {
                     let c = get_smaller_condition(g1.clone().into(), g2.clone().into());
-                    let g1 = aux(gs, i);
-                    let g2 = aux(gs2, j);
-                    return Some((c.clone(), g1, g2));
+                    let (g1, g1_aux) = Self::disj_except_for(gs, i);
+                    let (g2, g2_aux) = Self::disj_except_for(gs2, j);
+                    return Some((c.clone(), g1, g2, g1_aux, g2_aux));
                 }
             }
         }
@@ -78,14 +86,44 @@ impl FindITETransform {
                 while let Some(g) = gs.pop() {
                     let mut found = None;
                     for (i, g2) in gs.iter().enumerate() {
-                        if let Some((c, g, g2)) = self.find_match(&g, g2) {
-                            found = Some((i, c, g, g2));
+                        if let Some((c, g, g2, g1_aux, g2_aux)) = self.find_match(&g, g2) {
+                            found = Some((i, c, g, g2, g1_aux, g2_aux));
                             debug!("found");
                             break;
                         }
                     }
-                    if let Some((i, c, g, g2)) = found {
+                    if let Some((i, c, mut g, mut g2, g1_aux, g2_aux)) = found {
                         gs.remove(i);
+                        // handle (g1 \/ g2) /\ (not(g1) \/ g3) /\ (g1 \/ g4) /\ (not(g1) \/ g5)
+                        // to if (not(g1)) (g2 /\ g4) else g3 /\ g5
+                        let mut gs_new = Vec::new();
+                        for g3 in gs {
+                            let mut flag = 0;
+                            let mut target = None;
+                            for (i, c) in g3.iter().enumerate() {
+                                if c == &g1_aux {
+                                    flag = 1;
+                                    target = Some(i);
+                                    break;
+                                } else if c == &g2_aux {
+                                    flag = 2;
+                                    target = Some(i);
+                                    break;
+                                }
+                            }
+                            if flag == 1 {
+                                let (g3, _) = Self::disj_except_for(&g3, target.unwrap());
+                                g = Goal::mk_conj_opt(g, g3)
+                            } else if flag == 2 {
+                                let (g3, _) = Self::disj_except_for(&g3, target.unwrap());
+                                g2 = Goal::mk_conj_opt(g2, g3)
+                            } else {
+                                gs_new.push(g3)
+                            }
+                        }
+                        gs = gs_new;
+                        let g = self.f(&g);
+                        let g2 = self.f(&g2);
                         gs2.push(Goal::mk_ite_opt(c, g, g2));
                     } else {
                         gs2.push(
@@ -184,4 +222,53 @@ fn test_transform() {
         }
         _ => assert!(false),
     }
+}
+
+#[test]
+fn test_nested_if() {
+    use crate::formula::{Constraint, Ident, Op};
+    use hes::Goal;
+    // (x < 0 \/ F x) /\ (x >= 0 \/ x = 0 \/ y > 0) /\ (x >= 0 \/ x != 0 \/ y < 0)
+    let xi: Ident = Ident::fresh();
+    let x = Op::mk_var(xi);
+    let yi: Ident = Ident::fresh();
+    let y = Op::mk_var(yi);
+    let zero = Op::mk_const(0);
+    let func = Ident::fresh();
+
+    let c1 = Constraint::mk_lt(x.clone(), zero.clone());
+    let a11 = Goal::mk_constr(c1);
+    let a12 = Goal::mk_app(Goal::mk_var(func), Goal::mk_var(xi));
+    let g1 = Goal::mk_disj(a11.clone(), a12.clone());
+
+    let c2 = Constraint::mk_geq(x.clone(), zero.clone());
+    let c3 = Constraint::mk_eq(x.clone(), zero.clone());
+    let c4 = Constraint::mk_gt(y.clone(), zero.clone());
+    let a21 = Goal::mk_constr(c2);
+    let a22 = Goal::mk_constr(c3);
+    let a23 = Goal::mk_constr(c4);
+    let g2 = Goal::mk_disj(a21.clone(), a22.clone());
+    let g2 = Goal::mk_disj(g2, a23);
+
+    let c5 = Constraint::mk_geq(x.clone(), zero.clone());
+    let c6 = Constraint::mk_neq(x.clone(), zero.clone());
+    let c7 = Constraint::mk_lt(y.clone(), zero.clone());
+    let a31 = Goal::mk_constr(c5);
+    let a32 = Goal::mk_constr(c6);
+    let a33 = Goal::mk_constr(c7);
+    let g3 = Goal::mk_disj(a31.clone(), a32.clone());
+    let g3 = Goal::mk_disj(g3, a33);
+
+    let a = Goal::mk_conj(g1, g2);
+    let g = Goal::mk_conj(a, g3);
+
+    let trans = FindITETransform { lightweight: false };
+
+    println!("before: {g}");
+    let g = trans.f(&g);
+    println!("after: {g}");
+    assert!(g.is_ite());
+    let g = trans.f(&g);
+    println!("after after: {g}");
+    assert!(g.is_ite());
 }
