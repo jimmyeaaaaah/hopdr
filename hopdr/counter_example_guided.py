7f8c9258c7ce5bd93d512232694a28da323fe3ff
jimmyeaaaaah <yurikahirobe@gmail.com>
2024-07-26 10:51:08 +0900

    modify inequality solver from PuLP to Z3

--- hopdr/counter_example_guided.py
+++ hopdr/counter_example_guided.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sympy as sp
-import pulp
+from z3 import *
 import os
 import re
 import sys
@@ -8,7 +8,6 @@ import subprocess
 import time
 
 n_constraints = 0
-args = []
 n_iter = 1
 n_rf = 0
 
@@ -25,7 +24,7 @@ def get_n_rf(filename):
             n_rf += 1
     return n_rf
 
-def apply_new_ranking_function(filename, ranking_functions, is_first=False):
+def apply_new_ranking_function(filename, ranking_functions, args, is_first=False):
     print(f"ranking function for iter {n_iter} : {ranking_functions}")
     try:
         with open(filename, 'r') as file:
@@ -46,14 +45,17 @@ def apply_new_ranking_function(filename, ranking_functions, is_first=False):
 
     # RFの引数の変数のリストを取得する
     if is_first:
-        rf_line_terms = lines[-1].split()
-        for term in rf_line_terms:
-            if term.startswith("RF"):
-                continue
-            if term == "=v":
-                break
-            args.append(term)
-
+        rf_idx = 0
+        for line in lines:
+            if line.startswith("RF"):
+                terms = line.split()
+                for term in terms:
+                    if term.startswith("RF"):
+                        rf_idx = 0 if term[2:] == "" else int(term[2:])-1
+                        continue
+                    if term == "r":
+                        break
+                    args[rf_idx].append(term)
 
 def run_rethfl(filename):
     rethfl_cmd = f"rethfl --remove-disjunction {filename}"
@@ -118,47 +120,49 @@ def set_constraints(coes, problem, variables):
         coe1 = coes[i]
         coe2 = coes[i+1]
         coe = coe1 - coe2
-        problem += sum(coe[j] * variables[j] for j in range(n_term)) >= 1, f"Constraint_diff{n_constraints}"
+        problem.add(Sum([coe[j] * variables[j] for j in range(n_term)]) >= 1)
         n_constraints+=1
-        problem += sum(coe1[j] * variables[j] for j in range(n_term)) >= 0, f"Constraint_posi{n_constraints}"
+        problem.add(Sum([coe1[j] * variables[j] for j in range(n_term)]) >= 0)
         n_constraints+=1
-    problem += sum(coes[-1][j] * variables[j] for j in range(n_term)) >= 0, f"Constraint_posi{n_constraints}"
+    problem.add(Sum([coes[-1][j] * variables[j] for j in range(n_term)]) >= 0)
     n_constraints+=1
 
-def update_ranking_function(problem, variables):
+def update_ranking_function(problem, variables, args):
     new_rf = ""
     n_variable = len(variables)
 
     # print(problem)
 
     # 不等式制約を解く
-    solver = pulp.PULP_CBC_CMD(msg=False)   # ログを非表示
-    problem.solve(solver)
-
-    if pulp.LpStatus[problem.status] == 'Optimal':
+    if problem.check() == sat:
+        model = problem.model()
         for i in range(n_variable):
-            new_coe = variables[i].varValue
-            if(new_coe == None or int(new_coe) == 0):
+            new_coe = model[variables[i]]
+            if(new_coe == None or new_coe.as_long() == 0):
                 continue
             if(i != n_variable-1):
-                new_rf += f"({int(new_coe)}) * {args[i]} + "
+                new_rf += f"({new_coe.as_long()}) * {args[i]} + "
             else:
-                new_rf += f"({int(new_coe)}) + "
+                new_rf += f"({new_coe.as_long()}) + "
         new_rf = "1" if new_rf == "" else new_rf[:-3]
         new_rf = str(sp.sympify(new_rf))
     else:
         print("\nResult: No solution found.")
+        end_time = time.perf_counter_ns()
+        elapsed_time = (end_time - start_time)/ 1_000_000_000
+        print(f"total: {elapsed_time:.6f} sec")
         sys.exit(0)
+
     return new_rf
 
-def iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list):
+def iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list, args):
     global n_iter
     if(n_iter == 1):
         is_first = True
     else:
         is_first = False
     # ranking functionの初期値を設定
-    apply_new_ranking_function(filename, rf_list, is_first=is_first)
+    apply_new_ranking_function(filename, rf_list, args, is_first=is_first)
 
     # rethfl/show_traceを実行して結果を取得
     result = solve_nuhfl(filename)
@@ -170,9 +174,8 @@ def iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list):
         # LpProblemをranking functionの個数分用意
         n_variable = len(call_sequence[0])
         problem_name = f"RF{rf_idx}"
-        problem = pulp.LpProblem(problem_name, pulp.LpMaximize)
-        variables = [pulp.LpVariable(f'x{i}', lowBound=None, cat='Integer') for i in range(1, n_variable+1)]
-        problem += 0, "Objective_Function"      # 目的関数は無し
+        problem = Solver()
+        variables = [Int(f'x{i}') for i in range(1, n_variable+1)]
 
         problems[rf_idx] = problem
         variables_list[rf_idx] = variables
@@ -182,7 +185,7 @@ def iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list):
     set_constraints(call_sequence, problems[rf_idx], variables_list[rf_idx])
    
     # 不等式を解いてranking_functionを更新
-    new_rf = update_ranking_function(problems[rf_idx], variables_list[rf_idx])
+    new_rf = update_ranking_function(problems[rf_idx], variables_list[rf_idx], args[rf_idx])
     rf_list[rf_idx] = new_rf
     print("")
     n_iter+=1
@@ -194,12 +197,13 @@ def main():
 
     n_rf = get_n_rf(filename)
     rf_list = ["1"] * n_rf
-    unseen_rf = [True]*n_rf
-    problems = [False]*n_rf
-    variables_list = [False]*n_rf
+    unseen_rf = [True] * n_rf
+    problems = [False] * n_rf
+    variables_list = [False] * n_rf
+    args = [[] for _ in range(n_rf)]
 
-    while n_iter <= 20:
-        rf_list = iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list)
+    while n_iter <= 100:
+        rf_list = iteration(filename, rf_list, n_rf, unseen_rf, problems, variables_list, args)
         
 if __name__ == "__main__":
     start_time = time.perf_counter_ns()
